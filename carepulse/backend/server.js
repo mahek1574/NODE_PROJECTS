f974ad2c0fbe727ea8c2c558464d87c519a723ca
mahek1574 <shanishvaramahek1574@gmail.com>
2026-07-02 10:50:47 +0530

    carepulse update

--- carepulse/backend/server.js
+++ carepulse/backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ['http://localhost:5173','https://carepulse-azure-nine.vercel.app/'],
+    origin: ['http://localhost:5173','https://carepulse-azure-nine.vercel.app'],
     credentials: true
 }));
 app.use(session({
